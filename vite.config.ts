07c42fee7485b6fc278dfd41a26194e30a26b6a3
Colin Buffum <colin@direqt.ai>
2025-02-15 13:34:50 -0500

    refactor: remove SQLite composable and related types; delete TestSQL view

--- vite.config.ts
+++ vite.config.ts
@@ -10,17 +10,10 @@ export default defineConfig({
     server: {
         host: true,
         port: 3000,
-        headers: {
-            'Cross-Origin-Opener-Policy': 'same-origin',
-            'Cross-Origin-Embedder-Policy': 'require-corp',
-        },
     },
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
-    },
-    optimizeDeps: {
-        exclude: ['@sqlite.org/sqlite-wasm'],
     },
 });
