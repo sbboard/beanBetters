ca9e49729adafb146c17699ae6b31b907356809e
Colin Buffum <colin@direqt.ai>
2025-03-14 22:04:45 -0400

    feat: add house invites management to admin view with fetching and generation functionality

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,17 +1,17 @@
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user';
 import axios from 'axios';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const message = ref('');
 const api = import.meta.env.VITE_API;
 const userStore = useUserStore();
+const houseInvites = ref([]);
 
 const sendNotification = async () => {
     if (!message.value) return;
-
     try {
-        await axios.post(`${api}/mass-notification`, {
+        await axios.post(`${api}/admin/mass-notification`, {
             userId: userStore.user?._id,
             userKey: userStore.key,
             message: message.value,
@@ -22,6 +22,47 @@ const sendNotification = async () => {
         console.error('Error sending notification:', error);
     }
 };
+
+const getHouseInvites = async () => {
+    try {
+        const { data } = await axios.get(`${api}/admin/house-invites`, {
+            params: {
+                userId: userStore.user?._id,
+                userKey: userStore.key,
+            },
+        });
+        const invites = data.invites;
+        houseInvites.value = invites.map(
+            (invite: { meta: unknown }) => invite.meta
+        );
+    } catch (error) {
+        console.error('Error fetching house invites:', error);
+    }
+};
+
+const generateHouseInvite = async () => {
+    try {
+        const { data } = await axios.post(`${api}/admin/house-invites`, {
+            userId: userStore.user?._id,
+            userKey: userStore.key,
+        });
+        const invites = data.invites;
+        houseInvites.value = invites.map(
+            (invite: { meta: unknown }) => invite.meta
+        );
+    } catch (error) {
+        console.error('Error fetching house invites:', error);
+    }
+};
+
+onMounted(() => {
+    if (userStore.user?.role === 'admin') {
+        getHouseInvites();
+        return;
+    }
+    alert('Stop trying to be sneaky!');
+    window.location.href = '/';
+});
 </script>
 
 <template>
@@ -35,6 +76,10 @@ const sendNotification = async () => {
             ></textarea>
             <button style="width: 100px" @click="sendNotification">Send</button>
         </div>
+        <hr />
+        <h1>House Invites</h1>
+        {{ houseInvites }}
+        <button @click="generateHouseInvite">Generate House Invite</button>
         <hr />
         <h1>Ban Wager</h1>
     </div>
